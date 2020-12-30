71a3d3d5274d3605fcaa4d42cff7aa3ecb8576dc
jeano <kkangboss@gmail.com>
2020-12-30 17:22:30 +0900

    Truffe Init & config

---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  networks: {
+    development :{
+      host:'127.0.0.1',
+      port:8545,
+      network_id:'*'
+    }
+  },
+
+  compilers: {
+    solc: {
+      version: '0.5.0',
+      settings: {
+        optimizer: {
+          enabled: false,
+          runs: 200
+        }
+      }
+    }
+  }
+};
