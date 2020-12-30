bb2554672f90cfc85144bbdacd221739e30f7aea
jeano <kkangboss@gmail.com>
2020-12-30 18:07:31 +0900

    Transaction Faile처리에 대한 테스트 진행방법

--- test/mytoken.test.js
+++ test/mytoken.test.js
@@ -17,6 +17,11 @@ contract("My test", async function([user1,user2,user3]){
             assert.equal(Symbol, 'TKN', '토큰의 심볼이 일치하지 않습니다.')
         })
 
+        it("배포한 컨트랙트의 데시멀은 '18' 입니까?", async function() {
+            const decimal = await this.contract.decimals()
+            assert.equal(decimal, 18, 'decimal이 일치하지 않습니다.')
+        })
+
         it("컨트랙트가 배포되면, 총발행량은 10000000000 여야한다",async function(){
             const TotalSupply = await this.contract.totalSupply()
             assert.equal(TotalSupply.toString(),web3.utils.toWei('10000000000','ether').toString(),"토큰의 총 발행량이 일치하지 않습니다.")
@@ -29,13 +34,19 @@ contract("My test", async function([user1,user2,user3]){
         })
 
         it("User1이 User2에게 100개의 토큰을 전송하면, User1의 잔액이 -100으로 변경되어야 한다", async function(){
-
             const beforTransfer = await this.contract.balanceOf(user1);
-
             await this.contract.transfer(user2, 100,{from: user1})
-            
             const afterTransfer = await this.contract.balanceOf(user1);
             assert(afterTransfer,beforTransfer-100 ,'잔액이 일치하지 않습니다.');
+        })
+
+        it("0X0 주소로 전송하면 트랜잭션은 실패되어야 한다", async function(){
+            try{
+                await this.contract.transfer("0x0000000000000000000000000000000000000000", 100,{from: user1})
+            } catch(e){
+                return
+            }
+            assert(false,'트랜잭션은 실패되지 않음')
         })
     })
 
